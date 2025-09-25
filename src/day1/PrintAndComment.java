f5f3420ea0e2b8c0c0fbc147daaeaa717b68c138
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-09-25 21:42:46 +0530

    Data Types and Variables

---
+++ src/day1/PrintAndComment.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class PrintAndComment {
+    public static void main(String[] args) {
+
+        System.out.println(1); //1
+        //System.out.println(100);//100
+        System.out.println("Ashish");//Ashish
+        System.out.println('a');//a
+
+        //This is Java Class
+
+    }
+}
---
+++ src/day1/VariableConcepts.java
@@ -0,0 +1,33 @@
+package day1;
+
+public class VariableConcepts {
+    public static void main(String[] args) {
+
+        int x = 10;
+        double y = 12.34;
+        char c = 'a';
+        boolean b =true;
+
+        String myName= "Ashish";
+
+        String s ="I love Java";
+
+        int z;
+        z=200;
+        System.out.println(x);//10
+        System.out.println(s);// I Love Java
+        System.out.println(b);//true
+    }
+}
+
+
+//Primitive Data Types
+// byte/short/int/long ---> non-decimal number
+// float/double        ---> decimal numbers
+// char                ---> single character
+// boolean             ---> true/false
+
+
+//Non-Primitive Data Types
+//Arrays
+//String
